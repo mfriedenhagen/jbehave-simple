103e1a2114d3ac5b441d79eff080aa2bfa47ecd4
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-05-21 21:51:15 +0200

    Original definition

---
+++ src/main/java/de/friedenhagen/jbehavesimple/steps/MySteps.java
@@ -0,0 +1,29 @@
+package de.friedenhagen.jbehavesimple.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class MySteps {
+
+    @Given("I am a pending step")
+    public void givenIAmAPendingStep() {
+        // PENDING
+    }
+
+    @Given("I am still pending step")
+    public void givenIAmStillPendingStep() {
+        // PENDING
+    }
+
+    @When("a good soul will implement me")
+    public void whenAGoodSoulWillImplementMe() {
+        // PENDING
+    }
+
+    @Then("I shall be happy")
+    public void thenIShallBeHappy() {
+        // PENDING
+    }
+}
